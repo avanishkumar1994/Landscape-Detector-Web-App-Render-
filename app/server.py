5fe03ea4808318a18742021e3457832254dd5720
avanishkumar1994 <59700126+avanishkumar1994@users.noreply.github.com>
2020-05-21 12:29:24 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://www.googleapis.com/drive/v3/files/1C2tcnsCcK87i8sgtHzoquJjapHxL2tYG?alt=media&key=AIzaSyBw2UXIZBrzGktNwFmSFqmZQsI08pj245I'
-export_file_name = '1C2tcnsCcK87i8sgtHzoquJjapHxL2tYG.pth'
+export_file_name = 'export.pkl'
 
 classes = ['character_29_waw', 'character_6_cha', 'character_32_patalosaw', 'character_7_chha', 'character_4_gha', 'character_24_bha', 'character_12_thaa', 'character_22_pha', 'character_5_kna', 'character_10_yna', 'character_1_ka', 'character_27_ra', 'character_13_daa', 'digit_2', 'character_21_pa', 'digit_5', 'character_8_ja', 'digit_4', 'digit_3', 'character_3_ga', 'character_18_da', 'character_28_la', 'character_19_dha', 'character_23_ba', 'character_33_ha', 'character_2_kha', 'character_34_chhya', 'character_9_jha', 'character_11_taamatar', 'character_25_ma', 'character_30_motosaw', 'character_17_tha', 'character_35_tra', 'character_36_gya', 'character_31_petchiryakha', 'character_15_adna', 'character_14_dhaa', 'character_16_tabala', 'digit_6', 'digit_1', 'digit_8', 'character_20_na', 'character_26_yaw', 'digit_9', 'digit_0', 'digit_7']
 path = Path(__file__).parent
